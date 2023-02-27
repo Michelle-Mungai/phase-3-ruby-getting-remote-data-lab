5630aeb5f7c4c5f26f39937839af02d8cc5cd70c
Michelle_Mungai <chellymungai@gmail.com>
2023-02-27 11:51:56 +0300

    Completed assignment

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,21 @@
 # Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester
+  def initialize(str)
+    @str = str
+    @uri = ''
+  end
+
+  def get_response_body
+    @uri = URI.parse(@str)
+    @uri.open.string
+  end
+
+  def parse_json
+    JSON.parse(Net::HTTP.get_response(URI.parse(@str)).body)
+  end
+end
+
